a7e841c0f905ffce5adfbae96f27258f0e5abc05
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-13 12:02:04 -0700

    Add fifth exercise from afternoon object drills

--- arrays-of-objects.js
+++ arrays-of-objects.js
@@ -3,22 +3,33 @@
 const elf = {
   name: 'Legolas',
   jobTitle: 'archer',
+  boss: 'Gandalf',
 };
 
 const dwarf = {
   name: 'Gimli',
   jobTitle: 'axe-man',
+  boss: 'Gandalf',
 };
 
 const man = {
   name: 'Aragorn',
   jobTitle: 'swordsman',
+  boss: 'Gandalf',
 };
 
-const fellowship = [elf, dwarf, man];
+const wizard = {
+  name: 'Gandalf',
+  jobTitle: 'magicky-dude',
+};
+
+const fellowship = [elf, dwarf, man, wizard];
 
 function getInfo(arr) {
-  arr.forEach(el => console.log(`${el.name} is the ${el.jobTitle}`));
+  arr.forEach(el => {
+    const reportsTo = `${el.boss ? `reports to ${el.boss}.` : 'doesn\'t report to anybody.'}`
+    console.log(`${el.name} the ${el.jobTitle} ${reportsTo}`);
+  });
 }
 
 getInfo(fellowship);
