dc1b2cc068e513ae2e6ef849572652978199e335
Simon Chae <s13chae@gmail.com>
2018-06-13 09:06:05 -0700

    first exercise complete

---
+++ object-drills-1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// function createMyObject() {
+//   return {
+//     foo: 'bar',
+//     answerToUniverse: 42,
+//     'olly olly': 'oxen free',
+//     sayHello: function() {
+//       return 'hello';
+//     }
+//   };
+// }
+
+// const myObject = createMyObject();
+
+// console.log(myObject.sayHello());
