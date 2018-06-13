da19cc8b7f0ceb60ca34aad4a90800bc2850dcc0
Simon Chae <s13chae@gmail.com>
2018-06-13 09:28:09 -0700

    third exercise complete

--- object-drills-1.js
+++ object-drills-1.js
@@ -1,5 +1,8 @@
 'use strict';
 
+
+// ******** Object creator ************
+
 // function createMyObject() {
 //   return {
 //     foo: 'bar',
@@ -16,28 +19,51 @@
 // console.log(myObject.sayHello());
 
 
-
 // ******** Object updater ************
-function updateObject(obj) {
-  // First version:
-  // const tempObj = {
-  //   foo: 'foo',
-  //   bar: 'bar',
-  //   bizz: 'bizz',
-  //   bang: 'bang',
-  // };
-  // return Object.assign(obj, tempObj);
 
-  // Cleaner(?) version:
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj.bizz = 'bizz';
-  obj.bang = 'bang';
+// function updateObject(obj) {
+//   // First version:
+//   // const tempObj = {
+//   //   foo: 'foo',
+//   //   bar: 'bar',
+//   //   bizz: 'bizz',
+//   //   bang: 'bang',
+//   // };
+//   // return Object.assign(obj, tempObj);
 
-  return obj;
-}
+//   // Cleaner(?) version:
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj.bizz = 'bizz';
+//   obj.bang = 'bang';
 
-const orig = {hello: 'hello',};
-console.log(orig);
-updateObject(orig);
-console.log(orig);
+//   return obj;
+// }
+
+// const orig = {hello: 'hello',};
+// console.log(orig);
+// updateObject(orig);
+// console.log(orig);
+
+
+// ******** Self-reference ************
+
+// function personMaker() {
+//   var person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     // replace `null` with a function that uses self reference to return
+//     // full name
+//     fullName: function() {
+//       return `${this.firstName} ${this.lastName}`;
+//     },
+//   };
+//   return person;
+// }
+
+// const test = personMaker();
+
+// console.log(test.fullName());
+
+
+******** Self-reference ************
